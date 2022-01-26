52eb2a0e09f5aa17ec1419fc64cf97a38a50b6d8
Cheng sochio <98460440+sochio16@users.noreply.github.com>
2022-01-26 18:35:45 +0700

    How sum in c

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int sum;
+    printf("Input a: ");
+    scanf("%d",&a);
+    printf("Input b: ");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("%d",sum);
+    return 0;
+}
