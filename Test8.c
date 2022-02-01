9911e58e0f1b6cbb55a05298a602e168ed317842
Cheng sochio <98460440+sochio16@users.noreply.github.com>
2022-02-01 23:08:17 +0700

    test8.c

---
+++ Test8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a,b;
+    printf("Input A=");scanf("%d",&a);
+    printf("Input B=");scanf("%d",&b);
+    if(a>b)
+     printf("A is greater than B");
+    return 0;
+}
