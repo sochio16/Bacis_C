5395c23cf69389e08f87aa4c6f1592b9fcbb6e73
Cheng sochio <98460440+sochio16@users.noreply.github.com>
2022-02-01 22:50:32 +0700

    test7.c

--- test6.c
+++ test6.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
-{   int x, y,
-    printf("============Assign Value============\n");
-    x=18;
-    y=21;
-    printf(" %d > %d = %d\n",x,y,x>y);
-    printf(" %d >= %d = %d\n",x,y,x>=y);
-    printf(" %d < %d = %d\n",x,y,x<y);
-    printf(" %d <= %d = %d\n",x,y,x<=y);
-    printf(" %d == %d = %d\n",x,y,x==y);
-    printf(" %d != %d = %d\n",x,y,x!=y);
-    printf("--------------------------------\n");
-    printf("***Note: 1 = True   0 = False") ;
+{    int x, y;
+     printf("============Assign Value============\n");
+     x=18;
+     y=21;
+     printf(" %d > %d = %d\n",x,y,x>y);
+     printf(" %d >= %d = %d\n",x,y,x>=y);
+     printf(" %d < %d = %d\n",x,y,x<y);
+     printf(" %d <= %d = %d\n",x,y,x<=y);
+     printf(" %d == %d = %d\n",x,y,x==y);
+     printf(" %d != %d = %d\n",x,y,x!=y);
+     printf("--------------------------------\n");
+     printf("***Note: 1 = True   0 = False") ;
     return 0;
 }
---
+++ test7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    printf("----------Logic AND-------------\n");
+    printf("10 > 5 && 10==5     Answer = %d \n",10 > 5 && 10==5);
+    printf("10 > 5 && 10==10    Answer = %d \n",10 > 5 && 10==10);
+    printf("10 > 5 && 10==10    Answer = %d \n",10 < 5 && 10==10);
+    
+    printf("----------Logic OR-------------\n");
+    printf("10 > 5 || 10==5    Answer = %d \n",10 > 5 || 10==5);
+    printf("10 < 5 || 10==10   Answer = %d \n",10 < 5 || 10==10);
+    printf("10 < 5 || 10==10   Answer = %d \n",10 < 5 || 10==10);
+    printf("10 < 5 || 10!=10   Answer = %d \n",10 < 5 || 10!=10);
+    
+    printf("----------Logic !----------\n");
+    printf("!(10 > 5 || 10==5) Answer = %d \n",!(10 > 5 || 10==5));
+    printf("!(10 > 5 || 10==10) Answer = %d \n",!(10 > 5 || 10==10));
+    
+    printf("\n\n***note: 1 True      0 = False") ;
+    return 0;
+}
