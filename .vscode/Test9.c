0acb4477c8769cc78e31d724d8d2e6f5eecd40ca
Cheng sochio <98460440+sochio16@users.noreply.github.com>
2022-02-04 10:08:38 +0700

    test9.c

---
+++ .vscode/Test9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+      char status[20];
+      printf("Inpt Your Crush word[Yes/No]=");
+      scanf("%s",&status);
+       if(strcmp(status,"Yes")==0)
+       {
+           printf("Your Crush \3 you...!\n");
+           printf("I Love you too, Crush...!\n");
+           printf("I will promised you,\n");
+       }
+      printf("Sad, you don't Love me, crush....\n");
+      printf("I will hack your heart....!\n");
+      
+     return 0;
+}         
