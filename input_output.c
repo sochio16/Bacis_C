aeb66f452bd5074570d9997ff2358fdb2feba575
Cheng sochio <98460440+sochio16@users.noreply.github.com>
2022-02-08 17:00:24 +0700

    input output

---
+++ input_output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char name[20];
+    char sex;
+    int age;
+    float score;
+    printf("Input name=");
+    scanf("%s",name);
+    printf("your name %s",name);
+    
+ return 0;       
+}
