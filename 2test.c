3f2a1033e38662686f1b2cbd632e2341f888cbab
Cheng sochio <98460440+sochio16@users.noreply.github.com>
2022-02-08 13:11:53 +0700

    test.c

---
+++ 2test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+int id;
+char name[GO];
+float score(10,10,10);
+printf("Input Your ID   =");scanf("%d",&id):
+printf("Input Your Name =");gets(name);
+printf("Input Your score =");scanf("%f",&score);
+printf(" %d  %s  %f\n",id,name,score);
