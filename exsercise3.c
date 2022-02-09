4fcbb925782d227da8c3647e4cf9a108424cea17
Cheng sochio <98460440+sochio16@users.noreply.github.com>
2022-02-09 10:12:33 +0700

    example.c

---
+++ exsercise3.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int num;
+printf("Input Number=");
+scanf("%d",&num);
+if("number>0")[
+    printf("you Input positive numbe");
+]else if (num<0)[
+    printf("you Input Negative number");
+[elseo[
+    printf("you Input zero number);
+    
+    return 0;
+
+}
